49eeb9162f31485959f5d607a4bbb3dfe6f210eb
ajsiftw <ajsiftw@gmailcom>
2026-01-27 14:44:32 +0200

    14th commit workspace

--- src/test/java/testRunners/RunCucumberTest.java
+++ src/test/java/testRunners/RunCucumberTest.java
@@ -2,17 +2,15 @@ package testRunners;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-import org.junit.Test;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
-        features = {"C://Users//ajsif//IdeaProjects//project-java//src//test//features//Login.feature",
-                    "C://Users//ajsif//IdeaProjects//project-java//src//test//features//PracticeForm.feature"},
+        features = "classpath:features",
         glue = "stepDefinitions",
         dryRun = false,
         monochrome = true,
-        plugin = {"pretty", "html:test-output"}
+        plugin = {"pretty", "html:test-output.html"}
 )
 
 public class RunCucumberTest {
